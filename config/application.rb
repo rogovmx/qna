3bf773846cc167c5cc5ed519342782a4f1b40dd1
Mike <rogovmx@gmail.com>
2019-01-21 18:38:07 +0300

    Question with specs

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Qna
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+#    config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.fallbacks = [I18n.default_locale]
   end
 end
